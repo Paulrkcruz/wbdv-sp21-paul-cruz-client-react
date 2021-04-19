8948f908a11893c37379cdf49f5fcb1a0ae7258f
Paul Cruz <cruz.p@husky.neu.edu>
2021-04-19 14:08:33 -0700

    a8-final

--- src/services/quiz-service.js
+++ src/services/quiz-service.js
@@ -8,6 +8,15 @@ export const findQuizById = (quizId) =>
     fetch(`${QUIZZES_URL}/${quizId}`)
         .then(response => response.json())
 
+export const submitQuiz = (quizId, questions) => {
+    fetch(`http://localhost:4000/api/quizzes/${quizId}/attempts`, {
+        method: 'POST',
+        body: JSON.stringify(questions),
+        headers: {'content-type': 'application/json'}})
+        .then(response => response.json())
+        .then(result => console.log(result))
+}
+
 export default {
-    findAllQuizzes, findQuizById
+    findAllQuizzes, findQuizById, submitQuiz
 }
